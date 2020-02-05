2812f6db108dc7e8b9356a87109cbf11a011d5a1
Raul <raulliston@hotmail.com>
2020-02-04 21:35:29 -0300

    Inicial commit with a basic code to send strings via serial to lora that is transmitting

---
+++ main.py
@@ -0,0 +1,15 @@
+import serial
+import time
+
+ser = serial.Serial(port="COM4", baudrate=115000)  # open serial port
+while True:
+    ser.write(b":00000000000000000000000000000000000000000000000000000000000:\n")
+    time.sleep(0.1)
+    '''
+    ser.write(b":11111111111111111111111111111111111111111111111111111111111:\n")
+    time.sleep(0.1)'''
+
+
+ser.close()
+print(ser.name)         # check which port was really usedser.write(b'hello')     # write a string
+           # close port
