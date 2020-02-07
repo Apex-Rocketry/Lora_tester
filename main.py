c5cff4ff84558d68cfa59525188a4a5a03c8d182
Raul <raulliston@hotmail.com>
2020-02-07 16:37:04 -0300

    versão mais ou menos ok, algumas coisasas anotadas apra fazer dps

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ import time
 # pegar um pacote com tamanho definido e ir aumentando a frequencia de envio até falhar
 
 b = 0
-delay = 1
+delay = 0.1
 a= 0
 ser = serial.Serial(port="COM4", baudrate=115000)  # open serial port
 while b < 200:
     while a < 10:
-        message = "pacote: " + str(a) + " delay: " + str(delay) + "  00000000000000000000000000000000000000000000000000"
+        message = "pacote: " + str(a) + " delay: " + str(delay) + "\n"
         a += 1
         time.sleep(delay)
         ser.write(str(message).encode('ascii'))
         print(message)
     a = 0
     b += 1
-    delay = delay * 0.5
+    delay = delay * 0.9
 
 
 ser.close()
 print(ser.name)         # check which port was really usedser.write(b'hello')     # write a string
            # close port
+
+# diminuir airdatarate e ver quanto conseguimos mandar
