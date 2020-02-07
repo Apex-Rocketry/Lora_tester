03b329e3cac83f2d90b9713b41f097bf80686713
Raul <raulliston@hotmail.com>
2020-02-07 16:30:12 -0300

    basic

--- main.py
+++ main.py
@@ -5,20 +5,23 @@ import time
 
 b = 0
 delay = 1
-a= 0
-ser = serial.Serial(port="COM4", baudrate=115000)  # open serial port
+a = 0
+ser = serial.Serial(port="COM31", baudrate=115000)  # open serial port
 while b < 200:
-    while a < 10:
-        message = "pacote: " + str(a) + " delay: " + str(delay) + "  00000000000000000000000000000000000000000000000000"
-        a += 1
-        time.sleep(delay)
-        ser.write(str(message).encode('ascii'))
-        print(message)
-    a = 0
-    b += 1
-    delay = delay * 0.5
 
+    # message = "Pacote: " + str(b) + " Delay: " + str("{:.8f}".format(delay)) + "\n
+    if b < 10:
+        message = str(b) + "_1234560123456789012345678901234567890123456789012345678901234567890123456789\n"
+    elif 10 <= b < 100:
+        message = str(b) + "_123450123456789012345678901234567890123456789012345678901234567890123456789\n"
+    elif 100 <= b:
+        message = str(b) + "_12340123456789012345678901234567890123456789012345678901234567890123456789\n"
+
+    time.sleep(0.41)
+    ser.write(str(message).encode('ascii'))
+    print(message)
+    b += 1
 
 ser.close()
-print(ser.name)         # check which port was really usedser.write(b'hello')     # write a string
-           # close port
+print(ser.name)  # check which port was really usedser.write(b'hello')     # write a string
+# close port
