0792d187674fc1ff4ae9c2f9d5e71afa831f4d57
Raul <raulliston@hotmail.com>
2020-02-07 16:24:05 -0300

    Primeira versão funcional para teste

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ import time
 # pegar um pacote com tamanho definido e ir aumentando a frequencia de envio até falhar
 
 b = 0
-ser = serial.Serial(port="COM5", baudrate=115000)  # open serial port
-while True:
-    time.sleep(0.05)
+delay = 1
+a= 0
+ser = serial.Serial(port="COM4", baudrate=115000)  # open serial port
+while b < 200:
+    while a < 10:
+        message = "pacote: " + str(a) + " delay: " + str(delay) + "  00000000000000000000000000000000000000000000000000"
+        a += 1
+        time.sleep(delay)
+        ser.write(str(message).encode('ascii'))
+        print(message)
+    a = 0
     b += 1
-    a = str(b) +   "\n"
-    print(a)
-    ser.write(str(a).encode('ascii'))
-    '''
-    ser.write(b":11111111111111111111111111111111111111111111111111111111111:\n")
-    time.sleep(0.1)'''
+    delay = delay * 0.5
 
 
 ser.close()
