39f033eb57aaf64c8aefd9e9557c3787941f6326
Raul <raulliston@hotmail.com>
2020-02-08 21:55:10 -0300

    Versão usada para teste dem 500mts

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import serial
 import time
 
+
 # pegar um pacote com tamanho definido e ir aumentando a frequencia de envio até falhar
 
 b = 0
-delay = 0.1
+delay = 0.4
 a= 0
 ser = serial.Serial(port="COM4", baudrate=115000)  # open serial port
-while b < 200:
+while b < 400:
     while a < 10:
-        message = "pacote: " + str(a) + " delay: " + str(delay) + "\n"
+        message = "Sequencia: " + str(b) + " Pacote: " + str(a) + " Delay: " + str("{:.8f}".format(delay)) + "\n"
         a += 1
+        round(delay, 4)
         time.sleep(delay)
         ser.write(str(message).encode('ascii'))
         print(message)
     a = 0
     b += 1
-    delay = delay * 0.9
+    delay = delay * 0.8
 
 
 ser.close()
