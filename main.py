a5184caac452d1d3f45ebad5a10d2e9dbd6ad163
Raul <raulliston@hotmial.com>
2020-05-04 17:08:54 -0300

    Atualização do arquivo
    Código de repetição de mensagens usado para teste de transmissão.

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import serial
 import time
-
 # pegar um pacote com tamanho definido e ir aumentando a frequencia de envio até falhar
-
 b = 0
-delay = 1
-a = 0
-ser = serial.Serial(port="COM31", baudrate=115000)  # open serial port
+
+'''
+80 bytes:
+
+  19.2 Kbps: delay 0.2
+  9.6 Kbps: delay 0.28
+  4.8 Kbps: delay 0.42
+  2.4 Kbps: delay 0.3
+  1.2 Kbps: delay 0.4
+  0.3 Kbps: delay 0.5
+
+'''
+
+delay = 0.4 
+a= 0
+ser = serial.Serial(port="COM3", baudrate=115000)  # open serial port
 while b < 200:
-
-    # message = "Pacote: " + str(b) + " Delay: " + str("{:.8f}".format(delay)) + "\n
-    if b < 10:
-        message = str(b) + "_1234560123456789012345678901234567890123456789012345678901234567890123456789\n"
-    elif 10 <= b < 100:
-        message = str(b) + "_123450123456789012345678901234567890123456789012345678901234567890123456789\n"
-    elif 100 <= b:
-        message = str(b) + "_12340123456789012345678901234567890123456789012345678901234567890123456789\n"
-
-    time.sleep(0.41)
-    ser.write(str(message).encode('ascii'))
-    print(message)
+    while a < 10:
+        message = "Sequencia: " + str(b) + " Pacote: " + str(a) + " Delay: " + str("{:.2f}".format(delay)) + " _1234567891011121314151617181920212223242526\n"
+        a += 1
+        round(delay, 4)
+        time.sleep(delay)
+        ser.write(str(message).encode('ascii'))
+        print(message)
+    a = 0
     b += 1
-
 ser.close()
-print(ser.name)  # check which port was really usedser.write(b'hello')     # write a string
-# close port
+print(ser.name)         # check which port was really usedser.write(b'hello')     # write a string
+           # close port
+# diminuir airdatarate e ver quanto conseguimos mandar
